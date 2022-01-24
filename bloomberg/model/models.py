7d9bde6933cfe451c0ff4c45cfa6eca76f64424f
ChoiYoungMin <dudals3844@gmail.com>
2022-01-24 14:21:48 +0900

    PX_CLOSE_1D 일일 종가 데이터 모델 구현

--- bloomberg/model/models.py
+++ bloomberg/model/models.py
@@ -1,6 +1,7 @@
 from tkinter import image_names
 from numpy import var
-from sqlalchemy import Column, VARCHAR, DATETIME, PrimaryKeyConstraint
+from pandas import DataFrame
+from sqlalchemy import Column, VARCHAR, DATETIME, Float, PrimaryKeyConstraint
 from config.settings import BASE
 from datetime import datetime
 
@@ -12,5 +13,15 @@ class INDXMEMBERSModel(BASE):
     member_ticker_and_exchange_code = Column(VARCHAR(20))
     __table_args__ = (
         PrimaryKeyConstraint(security, date, member_ticker_and_exchange_code),
+        {},
+    )
+
+class PXCLOSE1DModel(BASE):
+    __tablename__ = "tb_px_close_1d"
+    date = Column(DATETIME)
+    ticker = Column(VARCHAR(20))
+    PX_CLOSE_1D = Column(Float)
+    __table_args__ = (
+        PrimaryKeyConstraint(date, ticker),
         {},
     )
--- migration.py
+++ migration.py
@@ -1,5 +1,6 @@
 from config.settings import ENGINE
-from bloomberg.model.models import INDXMEMBERSModel
+from bloomberg.model.models import INDXMEMBERSModel, PXCLOSE1DModel
 
 
 INDXMEMBERSModel.__table__.create(bind=ENGINE, checkfirst=True)
+PXCLOSE1DModel.__table__.create(bind=ENGINE, checkfirst=True)
