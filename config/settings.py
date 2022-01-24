390fa375f9a26e664f1140ec86af79646864b47d
ChoiYoungMin <dudals3844@gmail.com>
2022-01-24 15:28:05 +0900

    LOG 출력해주는 객체 구현

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,10 @@
+from tkinter.tix import IMMEDIATE
 from dotenv import load_dotenv
 import os
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from config.log import Logging
 
 load_dotenv()
 
@@ -13,3 +15,7 @@ SESSION = sessionmaker(bind=ENGINE)
 
 BASE = declarative_base()
 META = MetaData()
+
+
+LOG = Logging()
+
