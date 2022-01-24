3303c58cc72fc7cc7e9ef85358eacf86e6a4b63f
ChoiYoungMin <dudals3844@gmail.com>
2022-01-24 15:27:46 +0900

    객테 생성해주는 함수 구현

--- bloomberg/blp/__init__.py
+++ bloomberg/blp/__init__.py
@@ -1,6 +1,9 @@
 import imp
 from bloomberg.blp.INDX_MEMBERS import INDX_MEMBERS
 from bloomberg.blp.PX_CLOSE_1D import PX_CLOSE_1D
+from bloomberg.blp.PX_OPEN import PX_OPEN
+from bloomberg.blp.PX_HIGH import PX_HIGH
+from bloomberg.blp.PX_LOW import PX_LOW
 
 
 def get_indx_members(security, **kwargs):
@@ -9,3 +12,17 @@ def get_indx_members(security, **kwargs):
 
 def get_px_close_1d(security, **kwargs):
     return PX_CLOSE_1D(security=security, kwargs=kwargs)
+
+
+def get_px_open(security, **kwargs):
+    return PX_OPEN(security=security, kwargs=kwargs)
+
+
+def get_px_high(security, **kwargs):
+    return PX_HIGH(security=security, kwargs=kwargs)
+
+
+def get_px_low(security, **kwargs):
+    return PX_LOW(security=security, kwargs=kwargs)
+
+
