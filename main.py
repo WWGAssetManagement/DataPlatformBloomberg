06e38540b660f0b7b2d0c0ff36c3444c525f5ca6
ChoiYoungMin <dudals3844@gmail.com>
2022-01-24 14:23:58 +0900

    KOSPI Index 주식 가져와서 종가 가져오는것 구현

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import bloomberg
+from tqdm import tqdm
 
 if __name__ == "__main__":
-    print("Hello")
+    indx = bloomberg.get_indx_members(security="KOSPI Index")
+    data = indx.to_dict()
+    tickers = list(map(lambda x: x['member_ticker_and_exchange_code'] + ' EQUITY', data))
+    for ticker in tqdm(tickers):
+        price = bloomberg.get_px_close_1d(security=ticker, start="19800101", end="20220123")
+        price.to_mysql()
