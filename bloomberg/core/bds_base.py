b9db1f924d5aa67c17db4bfd725ef487a7009767
ChoiYoungMin <dudals3844@gmail.com>
2022-01-24 14:23:03 +0900

    데이터를 상속 받아서 수정하도록 변경

--- bloomberg/core/bds_base.py
+++ bloomberg/core/bds_base.py
@@ -11,27 +11,25 @@ class BDHBase(BloombergDatabase):
     kwargs = None
     results = None
 
-    def __init__(self, security, field, model, **kwargs):
+    def __init__(self, security, field, model, kwargs):
         super().__init__(model=model)
         self.security = security 
         self.field = field 
         self.kwargs = kwargs
-        self.results = self._get()
+        # self.results = self._get()
+
+    def _check_results(self):
+         if len(self.results) == 0:
+            raise Exception("blp.bds의 security명이나 field명이 잘못되었습니다.")
+        
 
     def _get(self):
         '''
-            데이터 가져오기 
+            데이터 가져오기
+            상속 받아서 오버라이딩 후 원하는 형식으로 전처리 하는 코드 만들기  
         '''
-        results = blp.bds(self.security, self.field)
-        if len(results) == 0:
-            raise Exception("blp.bds의 security명이나 field명이 잘못되었습니다.")
-        return results  
+        pass
     
-    def _data_preprocessing(self):
-        """
-        상속 받아서 오버라이딩 후 원하는 형식으로 전처리 하는 코드 만들기 
-        """
-        pass 
 
     def to_pandas(self):
         return self.results
