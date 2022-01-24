52cc57d92633935e1b7fbe934dc97f3354a498f1
ChoiYoungMin <dudals3844@gmail.com>
2022-01-24 15:27:29 +0900

    고가, 저가, 시가, 거래량 데이터 가져오는 코드 구현

---
+++ bloomberg/blp/PX_OPEN.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from bloomberg.core.blp_base import BLPBase
+from bloomberg.model.models import PXOPENModel
+from xbbg import blp
+
+class PX_OPEN(BLPBase):
+    """
+    blp.bhs에서 PX_OPEN 필드의 값입니다. 
+    """
+
+    def __init__(self, security, kwargs):
+        super().__init__(security, self.__class__.__name__, PXOPENModel, kwargs)
+        self._get()
+        self._check_results()
+
+    def _get(self):
+        self.results = blp.bdh(self.security, self.field, self.kwargs['start'], self.kwargs['end'])
+        self.results.columns.names = ['ticker', 'columns']
+        self.results = self.results.T.unstack('ticker').T 
+        self.results.index.names = ['date', 'ticker']
+        self.results = self.results.reset_index()
---
+++ bloomberg/blp/PX_VOLUME.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from bloomberg.core.blp_base import BLPBase
+from bloomberg.model.models import PXVOLUMEModel
+from xbbg import blp
+
+class PX_VOLUME(BLPBase):
+    """
+    blp.bhs에서 PX_VOLUME 필드의 값입니다. 
+    """
+
+    def __init__(self, security, kwargs):
+        super().__init__(security, self.__class__.__name__, PXVOLUMEModel, kwargs)
+        self._get()
+        self._check_results()
+
+    def _get(self):
+        self.results = blp.bdh(self.security, self.field, self.kwargs['start'], self.kwargs['end'])
+        self.results.columns.names = ['ticker', 'columns']
+        self.results = self.results.T.unstack('ticker').T 
+        self.results.index.names = ['date', 'ticker']
+        self.results = self.results.reset_index()
+ 
